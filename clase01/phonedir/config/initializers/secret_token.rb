22e3b7fae3766ca8cae8906d2fa26a499a3d98da
UPC_SEDE_SAN_BORJA <spantigo@upc.edu.pe>
2013-05-02 21:48:38 -0500

    Primer commit

---
+++ clase01/phonedir/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonedir::Application.config.secret_token = '2bc6df83b828ea6ec6c9bfb598eca4b6a0878a4d5e299fef59d6ccafde7305f8fc9611f1ca1f452a7a9300ae316709b9ebdbbe7230a184c002549e0bba3b7625'
